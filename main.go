b6d4ae2a5fe5178083b4c3833a845dabf4810908
Hamza Rafi <rafi.hamza2020@gmail.com>
2025-12-27 07:04:45 +0000

    download playlists using yt-dlp

--- main.go
+++ main.go
@@ -1,14 +1,20 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
+	"os/exec"
+	"path/filepath"
 
-	"github.com/davecgh/go-spew/spew"
 	"github.com/goccy/go-yaml"
 )
 
-var configFilePath string = "./config.yml"
+var (
+	configFilePath      string = "./config.yml"
+	archiveFilePath     string = "./archive.txt"
+	downloadsFolderPath string = "./downloads/"
+)
 
 func main() {
 	// load config from config file
@@ -22,5 +28,26 @@ func main() {
 		log.Fatalln(err)
 	}
 
-	spew.Dump(config)
+	for i, playlist := range config.Playlists {
+		fmt.Print("Downloading: ")
+		fmt.Printf("%d - %s - %s\n", i, playlist.Title, playlist.Link)
+
+		cmd := exec.Command(
+			"yt-dlp",
+			"-f", "bestvideo[height<=480]+bestaudio/best[height<=480]",
+			"--merge-output-format", "mp4",
+			"-o", filepath.Join(downloadsFolderPath, playlist.Title, "%(playlist_index)s - %(title)s.%(ext)s"),
+			"--download-archive", archiveFilePath,
+			"--concurrent-fragments", "4",
+			"--cookies-from-browser", "firefox:/home/hamza/.zen/449e99hw.Default (beta)",
+			playlist.Link,
+		)
+
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+
+		if err := cmd.Run(); err != nil {
+			log.Fatalln(err)
+		}
+	}
 }
